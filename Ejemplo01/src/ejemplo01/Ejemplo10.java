c10df4c37db4ae0ceef7b0109de6acf8356cac7a
Daniel Idrovo <doidrovo@utpl.edu.ec>
2024-10-30 12:56:31 -0500

    tab

--- Ejemplo01/src/ejemplo01/Ejemplo10.java
+++ Ejemplo01/src/ejemplo01/Ejemplo10.java
@@ -11,12 +11,12 @@ package ejemplo01;
  *
  * @author reroes
  */
-public class Ejemplo05 {
+public class Ejemplo10 {
     public static void main(String[] args) {
         // Secuencia de escape \n y \t
-        String nombreEstudiante = "René Rolando";
-        String apellidoEstudiante = "Elizalde Solano";
-        int nacimiento = 1983;
+        String nombreEstudiante = "Daniel Oswaldo";
+        String apellidoEstudiante = "Idrovo Bustos";
+        int nacimiento = 2006;
         
         /*System.out.println(nombreEstudiante+"\n"+apellidoEstudiante+ "\n"+ 
                 nacimiento);
@@ -26,7 +26,7 @@ public class Ejemplo05 {
         
         System.out.println(nombreEstudiante+"\n"+"\n"+apellidoEstudiante+ "\n"+
                 "\n"+ nacimiento); */
-        System.out.println(nombreEstudiante+"\t"+apellidoEstudiante);   
-        System.out.println(nombreEstudiante+"\n\n\t"+apellidoEstudiante);   
+        System.out.println(nombreEstudiante+"\n\t"+apellidoEstudiante+"\n\t\t"+nacimiento);   
+           
     }
 }
